9a645df7a6134c88efa82952e233b3c5a45742e4
NaveenIHUB <naveen.m.ihub@snsgroups.com>
2025-03-11 12:29:47 +0530

    completed

--- backend/server.mjs
+++ backend/server.mjs
@@ -14,7 +14,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    // origin: 'http://localhost:3000',
+    origin: '*',  // Allow requests from all domains
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   })
 );
